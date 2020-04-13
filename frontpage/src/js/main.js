574c1511539890c33e0c655921300d209f79fa29
Artem Aksenov <dutchnewsru@gmail.com>
2020-04-13 16:55:50 +0300

    I will leave my own menu here

---
+++ frontpage/src/js/main.js
@@ -0,0 +1,25 @@
+/*$(document).ready(function(){
+  $("#open-btn").click(function(){
+    $("#overlay").slideDown("slow");
+  });
+});
+
+$(document).ready(function(){
+
+$(".menulink a").on('click', function(e) {
+	console.log(this.hash);
+})
+});*/
+
+$(document).ready(function(){
+  $(".open-btn").click(function(){
+    $("p").hide();
+  });
+});
+
+/*var overlay = document.getElementById('overlay');
+var closeMenu = document.getElementById('close-btn');
+
+document.getElementById('open-btn').addEventListener('click', function (){
+	overlay.classList.add('show-menu');
+});*/
