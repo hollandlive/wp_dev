63442475137841b1deeb03676b4631173d204a69
Artemis A <dutchnewsru@gmail.com>
2021-12-18 20:40:49 +0100

    JS intense lessons shop - 2

---
+++ js/cart.js
@@ -0,0 +1,26 @@
+const cart = () => {
+
+    const cartBtn = document.getElementById('cart');
+    const cartModal = document.querySelector('.cart');
+    //this method is faster then the second one
+    const cartModalClose = cartModal.querySelector('.cart-close');
+    //const cartModal = document.querySelector('.cart-close');
+
+    const openCart = () => {
+        cartModal.style.display = 'flex';
+    }
+    const closeCart = () => {
+        cartModal.style.display = 'none';
+    }
+    // cartBtn.onclick = function () {
+    //     openCart();
+    // }
+
+    cartBtn.addEventListener('click', openCart);
+
+    cartModalClose.addEventListener('click', () => {
+        closeCart();
+    });
+}
+
+cart();
